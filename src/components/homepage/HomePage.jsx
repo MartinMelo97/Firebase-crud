5d2d92bbbe27a05f7f6b6a9d999850a95496e2d6
Martin Melo <nitram-210397@hotmail.com>
2019-03-11 16:14:47 -0600

    Create products in firebase database done

---
+++ src/components/homepage/HomePage.jsx
@@ -0,0 +1,87 @@
+import React, { Component } from 'react'
+import firebase from 'firebase'
+
+class HomePage extends Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            products: null,
+            newProduct: {
+                nombre: null,
+                desc: null,
+                cantidad: null
+            }
+        }
+    }
+    updateName=(e)=>{
+      //let nombre=this.state.newProduct.nombre
+      //let {nombre} = this.state.newProduct
+      let product = this.state.newProduct
+      product.nombre=e.target.value
+      this.setState({newProduct:product})
+    }
+    updateDesc=(e)=>{
+        let product = this.state.newProduct
+        product.desc=e.target.value
+        this.setState({newProduct:product})
+    }
+
+    updateCantidad = (e) => {
+        let product = this.state.newProduct
+        product.cantidad = e.target.value
+        this.setState({newProduct: product})
+    }
+
+    createProduct = (e) => {
+        firebase.firestore().collection('products_crud').add(this.state.newProduct)
+            .then(()=>{
+                alert("Producto añadido correctamente")
+                let copy = this.state.newProduct
+                copy.nombre = ""
+                copy.desc = ""
+                copy.cantidad = ""
+                this.setState({newProduct: copy})
+            })
+            .catch((err)=>{
+                alert("No se pudo crear el producto, checa la consola")
+                console.log(err)
+            })
+    }
+
+    render(){
+        return (
+            <div className="container">
+                <div className="products_container">
+
+                </div>
+
+                <div className="products_form">
+                    <p>Sube tu producto a la tiendita</p>
+
+                    <input type="text" 
+                        value={this.state.newProduct.nombre} 
+                        placeholder="Nombre" 
+                        onChange={(e)=>this.updateName(e)}
+                    />
+
+                    <input type="text" 
+                        value={this.state.newProduct.desc} 
+                        placeholder="Descripción"
+                        onChange={(e)=>this.updateDesc(e)}
+                    />
+
+                    <input type="number" 
+                        placeholder="Cantidad" 
+                        onChange={(e)=>this.updateCantidad(e)} 
+                        value={this.state.newProduct.cantidad} 
+                    />
+
+                    <button onClick={(e)=>this.createProduct(e)}>Subir</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default HomePage
--- src/index.js
+++ src/index.js
@@ -3,8 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import{ BrowserRouter } from 'react-router-dom'
+import firebase from 'firebase'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+firebase.initializeApp({
+        apiKey: "AIzaSyAfUID5SQ2qlWGTA8Jvdf_2nhUJDXoXHxo",
+        authDomain: "farmaciabackend2.firebaseapp.com",
+        databaseURL: "https://farmaciabackend2.firebaseio.com",
+        projectId: "farmaciabackend2",
+        storageBucket: "",
+        messagingSenderId: "1092955129378"
+    })
+
+ReactDOM.render(
+<BrowserRouter>
+<App />
+</BrowserRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
