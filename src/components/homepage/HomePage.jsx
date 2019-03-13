7bd0977030aea3eee9ebf08b0e222c2e20f8ce14
Martin Melo <nitram-210397@hotmail.com>
2019-03-13 16:00:49 -0600

    CRUD

--- src/components/homepage/HomePage.jsx
+++ src/components/homepage/HomePage.jsx
@@ -12,16 +12,28 @@ class HomePage extends Component {
                 nombre: null,
                 desc: null,
                 cantidad: null
-            }
+            },
+            action: "Subir",
+            idProductToEdit: null
         }
     }
-
+/*
+{
+    nombre: "aesdg",
+    desc: "sdgf",
+    cantidad: "sfdgf",
+    id: "SFHSHJHDG"
+}
+*/
     componentDidMount = () => {
         firebase.firestore().collection('products_crud')
             .onSnapshot((products)=>{
                 let products_array = []
                 products.forEach((product)=>{
-                    products_array.push(product.data())
+                    //product.id -> Aqui viene el nombre del documento (id)
+                    let product_object = product.data()
+                    product_object.id = product.id
+                    products_array.push(product_object)
                 })
                 this.setState({products: products_array, isProducts: true})
             })
@@ -48,7 +60,9 @@ class HomePage extends Component {
     }
 
     createProduct = (e) => {
-        firebase.firestore().collection('products_crud').add(this.state.newProduct)
+        if(this.state.action === "Subir")
+        {
+            firebase.firestore().collection('products_crud').add(this.state.newProduct)
             .then(()=>{
                 alert("Producto añadido correctamente")
                 let copy = this.state.newProduct
@@ -61,6 +75,47 @@ class HomePage extends Component {
                 alert("No se pudo crear el producto, checa la consola")
                 console.log(err)
             })
+        }
+        else //Actualizar el producto
+        {
+            firebase.firestore().collection('products_crud').doc(this.state.idProductToEdit)
+            .set(this.state.newProduct)
+            .then(()=>alert("Producto actualizado correctamente"))
+            .catch((err)=>{
+                alert("No se pudo actualizar el producto")
+                console.log(err)
+            })
+        }
+        
+    }
+
+    updateProduct = (e, id) => {
+        e.preventDefault()
+        let productToEdit = this.state.products.filter((product)=>product.id === id)[0]
+        console.log(productToEdit)
+        this.setState({newProduct: productToEdit, action: "Actualizar", idProductToEdit: id})
+    }
+
+    cancelUpdate = () => {
+        let copy = this.state.newProduct
+        copy.nombre = ""
+        copy.desc = ""
+        copy.cantidad = ""
+        copy.id = ""
+        this.setState({newProduct: copy, action: "Subir", idProductToEdit: null})
+    }
+
+    deleteProduct = (e, id) => {
+        e.preventDefault()
+        if(window.confirm("¿Estás seguro de querer eliminar este producto?"))
+        {
+            firebase.firestore().collection("products_crud").doc(id).delete()
+                .then(()=>alert("Producto eliminado correctamente"))            
+                .catch((err)=>{
+                    alert("No se puedo eliminar el producto")
+                    console.log(err)
+                })
+        }
     }
 
     render(){
@@ -69,9 +124,14 @@ class HomePage extends Component {
                 <div className="products_container">
                     {this.state.isProducts ?
                         this.state.products.map((product, index)=>(
-                            <p>Nombre: {product.nombre}    Descripcion: {product.desc}    Cantidad: {product.cantidad} </p>
+                            <div key={index}>
+                                <p>Nombre: {product.nombre}    Descripcion: {product.desc}    Cantidad: {product.cantidad} </p>
+                                <button onClick={(e)=>this.updateProduct(e, product.id)}>Editar</button>
+                                <button onClick={(e)=>this.deleteProduct(e, product.id)}>Eliminar</button>
+                            </div>
                         ))
-                        :null
+                        :
+                        <p>Cargando datos para ti papucho</p>
                     }
                 </div>
 
@@ -96,7 +156,13 @@ class HomePage extends Component {
                         value={this.state.newProduct.cantidad} 
                     />
 
-                    <button onClick={(e)=>this.createProduct(e)}>Subir</button>
+                    <button onClick={(e)=>this.createProduct(e)}>{this.state.action}</button>
+
+                    {
+                        this.state.action === "Actualizar" ? 
+                            <button onClick={this.cancelUpdate}>Perdón, la cagué, quiero subir producto</button>
+                            : null
+                    }
                 </div>
             </div>
         )
